c6dded96a9d66f9289c55758b001800b70bae0e7
WithLei <syunki@yeah.net>
2017-07-27 23:25:35 +0800

    ac

---
+++ HDU/2.2.5献给杭电五十周年校庆的礼物.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+int main(){
+	int range[1002];
+	
+	int input;
+	while(cin >> input)cout << range[input] << endl;
+	return 0;
+}
+/*
+1-2
+2-4
+3-8
+4-
+*/
