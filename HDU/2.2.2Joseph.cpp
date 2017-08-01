24350c9800904a3c44f6810f24e110bd7a5e3d64
WithLei <syunki@yeah.net>
2017-08-01 23:19:28 +0800

    ac

--- HDU/2.2.2Joseph.cpp
+++ HDU/2.2.2Joseph.cpp
@@ -1,19 +1,51 @@
-#include<cstdio>
-#include<iostream>
-using namespace std;
-bool work(int x,int n){
-	for(int i=1;i<=n;i++){
-		int temp = x*i;
-		if(temp%(2*n)<=n)return false;
-	}
-	return true;
-} 
-
-int main(){
-	int n;
-	while(cin >> n && n!=0){
-		int x=n+1;
-		while(!work(x,n)){x++;cout <<x <<endl;}
-		cout << x << endl;
-	}
-}
+#include<iostream>  
+#include<cstdio>  
+#include<cstring>  
+#include<iostream>  
+#include<cmath>  
+using namespace std;  
+int res[16];  
+  
+int fun(int n)  
+{  
+    int ans, flag, sum;  
+    if(res[n])  
+        return res[n];  
+    else  
+    {  
+        for(ans = n + 1; ; ++ans)  
+        {  
+            flag = 0;  
+            sum = 2 * n;  
+            for(int j = ans; ; j += ans - 1) //???1  
+            {  
+                if(j > sum)  
+                    j = j % sum ? j % sum : sum; //???1  
+                if(j <= n)   
+                    break;  
+                else      
+                    sum--;  
+                if(sum == n)  
+                {  
+                    flag = 1;  
+                    break;  
+                }  
+            }  
+            if(flag)  
+            {  
+                res[n] = ans;  
+                return res[n];  
+            }  
+        }  
+    }  
+}  
+  
+int main()  
+{  
+    int n;  
+    while(scanf("%d", &n) && n)  
+    {  
+        printf("%d\n", fun(n));  
+    }  
+    return 0;  
+}          
