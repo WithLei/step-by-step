c27e6a865012a2b7bce859cff80c8d45da3e829e
WithLei <syunki@yeah.net>
2017-09-25 22:45:54 +0800

    AC

--- Codeforces Round/Manthan, Codefest 17_B.cpp
+++ Codeforces Round/Manthan, Codefest 17_B.cpp
@@ -3,16 +3,25 @@ using namespace std;
 typedef long long LL;
 const int maxn = 100000+13;
 int main(){
-		LL n,a[4],num,result=0,maxnum=-99999999,minnum=99999999;
+		LL n,a[8],num=0;
 		cin >> n >> a[0] >> a[1] >> a[2];
-		for(int i=0;i< n;i++){
+		if(n){
 			cin >> num;
-			if(num>=maxnum)maxnum = num;
-			if(num<=minnum)minnum = num;
+			a[3]=a[0]*num;
+			a[4]=a[1]*num;
+			a[5]=a[2]*num;
 		}
-		for(int i=0;i<3;i++){
-			if(a[i]>=0)result+=a[i]*maxnum;
-			else result+=a[i]*minnum;
+		for(int i=1;i < n;i++){
+			cin >> num;
+			if(num*a[2]>a[5]){
+				a[5]=num*a[2];
+				if(num*a[1]>a[4]){
+					a[4]=num*a[1];
+					if(num*a[0]>a[3]){
+						a[3]=num*a[0];
+					}
+				}
+			}
 		}
-		cout << result << endl;
+		cout << a[3]+a[4]+a[5] << endl;
 }
