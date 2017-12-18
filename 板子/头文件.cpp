7c28f2a0f1421de3a3952ac8176ebcd2d41d363e
WithLei <syunki@yeah.net>
2017-12-18 22:02:40 +0800

    每天AC一点点

---
+++ 板子/头文件.cpp
@@ -0,0 +1,27 @@
+#include<map>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<vector>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+using namespace std;
+const int maxn = 1e5+4;
+
+int main(){
+	std::ios::sync_with_stdio(false);  
+    cin.tie(0);cout.tie(0);  
+	int cases;
+	cin >> cases;
+	while(cases--){
+		
+	}
+	return 0;
+}
